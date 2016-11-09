5263c1a652d3e04cc6807782b4619d893a7efc48
文超 <cstyles@wenchaodeMacBook-Pro.local>
2016-11-09 12:41:30 +0800

    更新开发进度 1. sass模块还有问题 文档过大会被截断 2. rollup vue处理还没想好

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 module.exports.filter = require('./lib/filter');
 module.exports.readFile = require('./lib/readFile');
+module.exports.rollup = require('./lib/rollup');
+module.exports.compile = require('./lib/compile');
 module.exports.rename = require('./lib/rename');
 module.exports.sprite = require('./lib/sprite');
 module.exports.tinify = require('./lib/tinify');
 module.exports.ftp = require('./lib/ftp');
-module.exports.utils = require('./lib/modules/utils');
+module.exports.sass = require('./lib/sass');
 module.exports.request = require('request');
 module.exports.del = require('del');
 module.exports.gutil = require('gulp-util');
-
-
-// module.exports.filter = require('./_/lib/filter');
-module.exports.compile = require('./_/lib/compile');
-// module.exports.tinify = require('./_/lib/tinify');
-// module.exports.sprite = require('./_/lib/sprite');
-module.exports.rollup = require('./_/lib/rollup');
-// module.exports.rename = require('./_/lib/rename');
-// module.exports.ftp = require('./_/lib/ftp');
-module.exports.readFiles = require('./_/lib/readFiles');
-// module.exports.utils = require('./_/lib/modules/utils');
-// module.exports.del = require('del');
-// module.exports.gutil = require('gulp-util');
-module.exports.noop = require('gulp-util').noop;
